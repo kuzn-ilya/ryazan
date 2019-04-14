a4db195dbef6c0378b883963ab4aebf2fafd68bc
Ilya Kuznetsov <Ilya_Kuznetsov@epam.com>
2019-04-14 18:12:50 +0300

    feat(map): determine location of a user

--- client/screens/MapScreen.tsx
+++ client/screens/MapScreen.tsx
@@ -1,22 +1,51 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationScreenConfigProps } from 'react-navigation'
-import { HOME } from './consts';
+import { MapView, PROVIDER_GOOGLE, Location, Permissions } from 'expo';
 import { styles } from './styles';
 
 export type MapScreenProps = {} & NavigationScreenConfigProps
 
-export class MapScreen extends React.Component<MapScreenProps> {
+interface MapScreenState {
+  location: Location.LocationProps;
+}
+
+export class MapScreen extends React.Component<MapScreenProps, MapScreenState> {
     static navigationOptions = {
       title: 'Map',
     };
+
+    state = {
+      location: {
+        latitude: 54.629216,
+        longitude: 39.736375
+      }
+    };
+
+    componentDidMount() {
+      this._getLocationAsync();
+    }
+
+    _getLocationAsync = async () => {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === 'granted') {
+        let locationData = await Location.getCurrentPositionAsync({ accuracy: 1 });
+        this.setState({ location: locationData.coords });
+      }
+    }
+
     render() {
-      const {navigate} = this.props.navigation;
       return (
         <View style={styles.container}>
-          <Button
-            title="Open list screen"
-            onPress={() => navigate(HOME)}
+          <MapView 
+            style={StyleSheet.absoluteFillObject}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: this.state.location.latitude,
+              longitude: this.state.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
           />
         </View>
       );
