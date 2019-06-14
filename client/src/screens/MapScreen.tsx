e259828ff29b3f3ab3699a3a5dd47f5cbe129516
Evgeny Rodin <eugene.rodin1@gmail.com>
2019-06-14 22:36:55 +0300

    chore(project-setup): src folder for client, formatting

---
+++ client/src/screens/MapScreen.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {NavigationScreenConfigProps} from 'react-navigation';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {styles} from './styles';
+
+export type MapScreenProps = {} & NavigationScreenConfigProps;
+
+type MapScreenState = {
+    location: Location.GeocodedLocation;
+};
+
+export class MapScreen extends React.Component<MapScreenProps, MapScreenState> {
+    static navigationOptions = {
+        title: 'Map',
+    };
+
+    state = {
+        location: {
+            latitude: 54.629216,
+            longitude: 39.736375,
+        },
+    };
+
+    async componentDidMount() {
+        await this._getLocationAsync();
+    }
+
+    _getLocationAsync = async () => {
+        try {
+            const {status} = await Permissions.askAsync(Permissions.LOCATION);
+            if (status === 'granted') {
+                let locationData = await Location.getCurrentPositionAsync({accuracy: 1});
+                this.setState({location: locationData.coords});
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    style={StyleSheet.absoluteFillObject}
+                    provider={PROVIDER_GOOGLE}
+                    initialRegion={{
+                        latitude: this.state.location.latitude,
+                        longitude: this.state.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
+        );
+    }
+}
