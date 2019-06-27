4fda49e0dd8b3607f999acbd25e608ee817ff563
lxndr <lxndr87i@gmail.com>
2019-06-27 23:00:16 +0300

    feat(map): added headers on map and poi screens

--- client/src/screens/Map/atoms.ts
+++ client/src/screens/Map/atoms.ts
@@ -1,10 +1,6 @@
 import styled from 'styled-components/native';
 import MapView from 'react-native-maps';
 
-export const Container = styled.View`
-    flex: 1;
-`;
-
 export const Map = styled(MapView)`
     flex: 1;
 `;
