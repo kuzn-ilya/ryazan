626e1dd35dd315314a394b026354be463b87c964
lxndr <lxndr87i@gmail.com>
2019-06-28 10:51:47 +0300

    feat(components): added title for window header

--- client/src/components/WindowHeader/atoms.ts
+++ client/src/components/WindowHeader/atoms.ts
@@ -7,3 +7,18 @@ export const Container = styled(Header).attrs({
 })`
     background-color: ${theme.windowHeaderColor};
 `;
+
+export const Content = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    margin-right: 60;
+`;
+
+export const Title = styled.Text`
+    font-family: Roboto;
+    font-size: 14;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: white;
+`;
--- client/src/components/WindowHeader/index.tsx
+++ client/src/components/WindowHeader/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {HeaderProps} from 'react-navigation';
 import {IconButton} from '../IconButton';
-import {Container} from './atoms';
+import {Container, Content, Title} from './atoms';
 
-export const WindowHeader: React.FC<HeaderProps> = ({navigation}) =>
+export const WindowHeader: React.FC<HeaderProps> = ({navigation, scene}) =>
     <Container>
         <IconButton
             icon="close"
             color="white"
             onPress={() => navigation.goBack(null)}
         />
+
+        <Content>
+            <Title>{scene.descriptor.options.title}</Title>
+        </Content>
     </Container>
